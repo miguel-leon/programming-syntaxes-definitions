aa0091cb90bd4b7096057ed3a51505aed97b68b0
Miguel Leon <miguel.e.leon.f@gmail.com>
2022-04-25 02:25:24 -0400

    ⚙🧪 complete tsconfig in file.

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,4 @@ module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
 	resolver: '<rootDir>/jest.resolver.js',
-	globals: {
-		'ts-jest': {
-			tsconfig: {
-				esModuleInterop: true,
-				resolveJsonModule: true
-			}
-		}
-	}
 };
